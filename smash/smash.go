8bbf883e698a2f26cf1d503d5c1fb7588f9b9973
Radish <53844660+radixsh@users.noreply.github.com>
2024-06-04 17:15:59 -0700

    Attempt using bufio

--- smash/smash.go
+++ smash/smash.go
@@ -2,12 +2,30 @@ package smash
 
 import (
 	"io"
+    "bufio"
 )
 
 type word string
 
+// {"foo", returnZero, map[uint32]uint{0: 1}},
+// The call "Smash(strings.NewReader("foo"), returnZero)" should return {0:1}
 func Smash(r io.Reader, smasher func(word) uint32) map[uint32]uint {
 	m := make(map[uint32]uint)
-	// TODO: Incomplete!
+
+    scanner := bufio.NewScanner(r)
+    scanner.Split(bufio.ScanWords)
+    
+    // For each word in r,
+    for scanner.Scan() {
+        // Call smasher() on that word
+        thing := strings.Fields(scanner.Scan()
+        to_append := smasher(word)
+        fmt.Println(to_append)
+
+        // Append the value that smasher() returns to the map m
+        m = append(m, to_append)
+    }
+
+
 	return m
 }
